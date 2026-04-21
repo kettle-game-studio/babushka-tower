cdc6a7d1040e81b3c7d68052877643d96caf7386
Tetrergeru <ArutunoffOleg@yandex.ru>
2026-04-21 02:40:40 +0200

    Add random phrases to dedushka

--- Assets/Scripts/Interactinators/Deadushka.cs
+++ Assets/Scripts/Interactinators/Deadushka.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -20,6 +18,19 @@ public class Deadushka : Interactinator
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
     }
+
+    string[] dudushkasStories = new string[]
+    {
+        "Today is a good day, Vnuchok, good day.",
+        "Have I ever told you how I met your Babushka? No? And i will not.",
+        "Never go inside our toilette, it's quite a dangerous place...",
+        "My komrades and I will have a soviet book club meeting tomorrow",
+        "I have built this house with my own two hands! And some tools stolen from the factory",
+        "Wonder where did my moonshine go... I remember there were couple of bottles",
+        "Good thing we live in the south, we can keep our windows open all day long",
+        "Do you think the can with pickles will shatter if you drop it from high enough?",
+        "Books are very good for your mind, even detective novels",
+    };
 
     void Update()
     {
@@ -57,8 +68,8 @@ public class Deadushka : Interactinator
         {
             audioSource.clip = randomClip(somethingClips);
             audioSource.Play();
-            player.Say("Say something", "You");
-            player.Say("Nothing to say", tooltip);
+            player.Say("How is your day, Dedushka?", "You");
+            player.Say(dudushkasStories[Random.Range(0, dudushkasStories.Length - 1)], this.tooltip);
         }
     }
 
